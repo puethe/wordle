14fa7e7df71f81e3aab28c2c08e0aefa0461e413
puethe <puethe@posteo.de>
2025-05-25 22:34:23 +0200

    refactoring: simplify domain code

--- src/domain.ts
+++ src/domain.ts
@@ -16,12 +16,13 @@ interface AttemptResult {
   overall: boolean;
 }
 
-interface LetterIntermediateResult {
+interface LetterTmpResult {
   isRemaining: boolean;
   value: string;
 }
 
 export const checkWord = (guess: string, answer: string): AttemptResult => {
+  // Check if guess is legitimate
   if (guess.length !== NB_CHARS) {
     throw new CustomError({
       message: `Invalid input. Guesses must have ${NB_CHARS} characters, input has ${guess.length} characters`,
@@ -29,49 +30,32 @@ export const checkWord = (guess: string, answer: string): AttemptResult => {
   }
 
   // Loop over the word a first time to find exact matches
-  const remainingLetters: Array<LetterIntermediateResult> = [];
-  const correctIndexes: Set<number> = new Set();
+  const wordResult: WordResult = answer.split('').map((_v, i) => {
+    return { index: i, correctAtPosition: false, presentElsewhere: false };
+  });
+  const remainingLetters: Array<LetterTmpResult> = answer.split('').map((v) => {
+    return { isRemaining: true, value: v };
+  });
   for (let i = 0; i < NB_CHARS; i++) {
     if (guess.charAt(i) === answer.charAt(i)) {
-      correctIndexes.add(i);
-      remainingLetters.push({ isRemaining: false, value: answer.charAt(i) });
-    } else {
-      remainingLetters.push({ isRemaining: true, value: answer.charAt(i) });
+      wordResult[i].correctAtPosition = true;
+      remainingLetters[i].isRemaining = false;
     }
   }
 
-  // Loop over the word a second time to find matches at different positions and build result
-  const wordResult: WordResult = [];
+  // Loop over the word a second time to find matches at different positions
   for (let i = 0; i < NB_CHARS; i++) {
-    if (correctIndexes.has(i)) {
-      const letterResult: LetterResult = {
-        index: i,
-        correctAtPosition: true,
-        presentElsewhere: false,
-      };
-      wordResult.push(letterResult);
-    } else {
-      const remIndex: number = remainingLetters.findIndex(
+    if (!wordResult[i].correctAtPosition) {
+      const remainingLetterIndex = remainingLetters.findIndex(
         (el) => el.isRemaining && el.value === guess.charAt(i),
       );
-      if (remIndex !== -1) {
-        const letterResult: LetterResult = {
-          index: i,
-          correctAtPosition: false,
-          presentElsewhere: true,
-        };
-        wordResult.push(letterResult);
-        remainingLetters[remIndex].isRemaining = false;
-      } else {
-        const letterResult: LetterResult = {
-          index: i,
-          correctAtPosition: false,
-          presentElsewhere: false,
-        };
-        wordResult.push(letterResult);
+      if (remainingLetterIndex !== -1) {
+        wordResult[i].presentElsewhere = true;
+        remainingLetters[remainingLetterIndex].isRemaining = false;
       }
     }
   }
+
   return {
     byLetter: wordResult,
     overall: wordResult.every((lr) => lr.correctAtPosition),
